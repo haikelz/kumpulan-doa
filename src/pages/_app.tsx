0fdfa40bf213b2211e0c85a042fc35c0d8965cd6
haikelz <haikel.2017th@gmail.com>
2022-07-30 09:24:32 +0700

    fix: forget to remove react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,12 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/font.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const queryClient: QueryClient = new QueryClient();
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 };
 
